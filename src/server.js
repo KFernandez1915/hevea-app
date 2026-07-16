1ab6614e5ee5c5ad2f05ffd4823418731e7cca03
KFernandez1915 <kkafer2.0@gmail.com>
2026-07-16 17:06:32 +0000

    Ajout de la route ping pour cron-job

--- src/server.js
+++ src/server.js
@@ -45,3 +45,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`[hevea-app] Serveur demarre sur http://localhost:${PORT}`);
 });
+// Route spéciale pour garder le serveur éveillé
+app.get('/ping', (req, res) => {
+  res.status(200).send('OK');
+});
